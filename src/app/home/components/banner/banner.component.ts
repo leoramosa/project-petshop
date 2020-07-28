f33c3db733c7acdba83cb75a8464f27abb910eb4
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-07-28 00:39:07 -0500

    change swiper no functional

--- src/app/home/components/banner/banner.component.ts
+++ src/app/home/components/banner/banner.component.ts
@@ -1,27 +1,18 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { ProductsService } from "../../../core/services/products/products.service";
 import { Product } from "../../../core/models/product.model";
-import Swiper from "swiper";
+
 @Component({
   selector: "app-banner",
   templateUrl: "./banner.component.html",
   styleUrls: ["./banner.component.scss"],
 })
-export class BannerComponent implements OnInit, AfterViewInit {
-  mySwiper: Swiper;
+export class BannerComponent implements OnInit {
   products: Product[] = [];
-  images: string[] = [
-    "assets/img/banner-1.jpg",
-    "assets/img/banner-2.jpg",
-    "assets/img/banner-3.jpg",
-  ];
+
   // por mientras
 
   constructor(private productService: ProductsService) {}
-
-  ngOnInit() {
-    this.fetchProducts();
-  }
 
   fetchProducts() {
     this.productService.getAllProducts().subscribe((products) => {
@@ -29,20 +20,7 @@ export class BannerComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit() {
-    this.mySwiper = new Swiper(".swiper-container.one", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    });
+  ngOnInit() {
+    this.fetchProducts();
   }
 }
