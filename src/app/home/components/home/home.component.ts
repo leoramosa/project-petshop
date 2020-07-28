74070ce039f47f4fc9649c1cab41c688ed7e7426
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-07-28 00:42:47 -0500

    change swiper no functional

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Inject, AfterViewInit } from "@angular/core";
-import Swiper from "swiper/bundle";
-import Glide, { Controls, Breakpoints } from "@glidejs/glide";
 
 @Component({
   selector: "app-home",
@@ -8,50 +6,9 @@ import Glide, { Controls, Breakpoints } from "@glidejs/glide";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  mySwiper: Swiper;
-  myGlide: Glide;
-
   constructor() {}
 
-  ngAfterViewInit(): void {
-    this.mySwiper = new Swiper(".swiper-container", {
-      // Optional parameters
-
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-
-      // If we need pagination
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-
-      // And if we need scrollbar
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
-    });
-    var sliders = document.querySelectorAll(".glide");
-
-    for (var i = 0; i < sliders.length; i++) {
-      var glide = new Glide(sliders[i], {
-        // options
-      });
-
-      glide.mount();
-    }
-  }
+  ngAfterViewInit(): void {}
 
   ngOnInit() {}
 }
